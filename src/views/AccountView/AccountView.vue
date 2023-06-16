6bf55077e6beafed02201c196ce2638623267b25
lee <1274666160@qq.com>
2023-06-16 19:04:29 +0800

    新增个人账户管理

---
+++ src/views/AccountView/AccountView.vue
@@ -0,0 +1,128 @@
+<template>
+  <div class="account">
+    <h1 style="color: black">个人账户管理</h1>
+    <el-table
+        :data="tableData"
+        border
+        style="width: 100%">
+      <el-table-column
+          fixed
+          prop="date"
+          label="日期"
+          width="150">
+      </el-table-column>
+      <el-table-column
+          prop="name"
+          label="姓名"
+          width="120">
+      </el-table-column>
+      <el-table-column
+          prop="province"
+          label="省份"
+          width="120">
+      </el-table-column>
+      <el-table-column
+          prop="city"
+          label="市区"
+          width="120">
+      </el-table-column>
+      <el-table-column
+        prop="authority"
+        label="权限管理"
+        width="120">
+        <el-switch
+            v-model="value"
+            :active-value="1"
+            :inactive-value="0"
+            active-color="#13ce66"
+            inactive-color="#a9a9a9">
+        </el-switch>
+    </el-table-column>
+      <el-table-column
+          prop="city"
+          label="市区"
+          width="120">
+      </el-table-column>
+
+      <el-table-column
+          prop="address"
+          label="地址"
+          width="300">
+      </el-table-column>
+      <el-table-column
+          prop="zip"
+          label="邮编"
+          width="120">
+      </el-table-column>
+      <el-table-column
+          fixed="right"
+          label="操作"
+          width="100">
+        <template slot-scope="scope">
+          <el-button @click="handleClick(scope.row)" type="text" size="small">编辑</el-button>
+          <el-button type="text" size="small">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+
+
+
+
+
+  </div>
+
+
+
+</template>
+
+<script>
+export default {
+  methods: {
+    handleClick(row) {
+      console.log(row);
+    }
+  },
+
+  data() {
+    return {
+      value:1,
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1518 弄',
+        zip: 200333
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1517 弄',
+        zip: 200333
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1519 弄',
+        zip: 200333
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        province: '上海',
+        city: '普陀区',
+        address: '上海市普陀区金沙江路 1516 弄',
+        zip: 200333
+      }]
+    }
+  }
+}
+</script>
+<style>
+
+</style>
+
+
+
