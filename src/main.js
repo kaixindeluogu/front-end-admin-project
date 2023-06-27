698a0d7448bcc6ca7a21609c51a44ead50e94187
lee <1274666160@qq.com>
2023-06-27 16:23:07 +0800

    mian.js上传qs

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import store from './store'
 import axios from 'axios'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import qs from 'qs';
 
+Vue.prototype.qs=qs;
 Vue.prototype.axios=axios;
 Vue.use(ElementUI);
 Vue.config.productionTip = false
