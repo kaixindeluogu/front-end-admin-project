bea115d9a51f0c299812c9729dcb4de986cfdaf9
lee <1274666160@qq.com>
2023-06-30 09:07:26 +0800

    OrderApproval

--- src/router/index.js
+++ src/router/index.js
@@ -6,92 +6,95 @@ import BookAdmin from "@/views/BookAdmin.vue";
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/home',
-    component: HomeView,
-    redirect: '/home/book',//重定向
-    children:[
-      {
-        path: 'about',// /home/about
-        component: () => import('../views/slideshow/AboutView.vue')
-      },
-      {
-        path: 'other',// /home/other
-        component: () => import('../views/slideshow/OtherView.vue')
-      },
-      {
-        path: '/home/report',// /home/report
-        component: () => import('../views/reportManagement/ReportView.vue')
-      },
+    {
+        path: '/home',
+        component: HomeView,
+        redirect: '/home/book',//重定向
+        children: [
+            {
+                path: 'about',// /home/about
+                component: () => import('../views/slideshow/AboutView.vue')
+            },
+            {
+                path: 'other',// /home/other
+                component: () => import('../views/slideshow/OtherView.vue')
+            },
+            {
+                path: '/home/report',// /home/report
+                component: () => import('../views/reportManagement/ReportView.vue')
+            },
 
-      {
-        path: 'dataStatistics',
-        component: () => import(/* webpackChunkName: "about" */ '../views/datastatisticsview/DataStatisticsView.vue')
-      },
-      {
-        path: 'book',
+            {
+                path: 'dataStatistics',
+                component: () => import(/* webpackChunkName: "about" */ '../views/datastatisticsview/DataStatisticsView.vue')
+            },
+            {
+                path: 'book',
+                // route level code-splitting
+                // this generates a separate chunk (about.[hash].js) for this route
+                // which is lazy-loaded when the route is visited.
+                component: () => import(/* webpackChunkName: "about" */ '../views/BookManagement/BookView.vue')
+            },
+            {
+                path: 'account',
+                component: () => import('../views/AccountView/AccountView.vue')
+            },
+            {
+                path: 'libraryApply',
+                component: () => import('../views/LibraryApply/LibraryApply.vue')
+            }
+
+        ]
+    },
+
+    {
+        path: '/bookadmin',
+        component: () => import('../views/BookAdmin.vue'),
+        children: [
+            {
+                path: 'librarybooks',
+                component: () => import('../views/BookAdminLibraryBooks/LibraryBooks.vue')
+            },
+            {
+                path: 'import',
+                component: () => import('../views/BookadminBookImport/ImportView.vue')
+            },
+            {
+                path: 'libraryRule',
+                component: () => import('../views/LibraryRule/LibraryRuleView')
+            },
+            {
+                path: 'ruleList',
+                component: () => import('../views/LibraryRule/RuleListView')
+            },
+            {
+                path: 'libraryDataStatistics',
+                component: () => import('../views/LibraryDataStatistics/LibraryDataStatistics.vue')
+            },
+            {
+                path: 'OrderApproval',
+                component: () => import('../views/OrderApproval/OrderApprovalView.vue')
+            }
+
+        ]
+    },
+
+
+    {
+        path: '/login',
         // route level code-splitting
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/BookManagement/BookView.vue')
-      },
-      {
-        path:'account',
-        component:() =>import('../views/AccountView/AccountView.vue')
-      },
-      {
-        path:'libraryApply',
-        component:() =>import('../views/LibraryApply/LibraryApply.vue')
-      },
-      {path:'OrderApproval',
-      component: () => import('../views/OrderApproval/OrderApprovalView.vue')}
-    ]
-  },
-
-  {
-    path: '/bookadmin',
-    component: () =>import('../views/BookAdmin.vue'),
-    children:[
-      {
-        path:'librarybooks',
-        component:() =>import('../views/BookAdminLibraryBooks/LibraryBooks.vue')
-      },
-      {
-        path:'import',
-        component:() =>import('../views/BookadminBookImport/ImportView.vue')
-      },
-      {
-        path:'libraryRule',
-        component:() =>import('../views/LibraryRule/LibraryRuleView')
-      },
-      {
-        path:'ruleList',
-        component:() =>import('../views/LibraryRule/RuleListView')
-      },
-      {
-        path:'libraryDataStatistics',
-        component:() =>import('../views/LibraryDataStatistics/LibraryDataStatistics.vue')
-      },
-       ]
-  },
-
-
-
-  {
-    path: '/login',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/LoginView.vue')
-  },
+        component: () => import(/* webpackChunkName: "about" */ '../views/LoginView.vue')
+    },
 
 
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
