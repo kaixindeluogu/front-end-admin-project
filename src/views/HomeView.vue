bdd24bdca787fcdb1a8f5fa7820c4ebb74283742
bianmy <1129729148@qq.com>
2023-07-11 10:16:29 +0800

    修改后端页面

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -52,10 +52,7 @@
               <i class="el-icon-setting"></i>
               <span slot="title">图书馆审核管理</span>
             </el-menu-item>
-            <el-menu-item index="/bookadmin">
-              <i class="el-icon-setting"></i>
-              <span slot="title">图书馆后台管理</span>
-            </el-menu-item>
+
 
           </el-menu>
         </el-aside>
